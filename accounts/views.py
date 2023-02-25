f55ab83e60b306d8c5f6c85122ec3f9f2f054b72
HSamiul <samiulh@andrew.cmu.edu>
2023-02-25 13:22:08 -0500

    Created a simple template file and connected it to the /accounts/signup URL

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def signup(request):
+    template = loader.get_template('accounts/signup.html')
+    
+    # The empty dictionary would normally be a context dictionary.
+    # It makes python variables available for use in the template file
+    return HttpResponse(template.render({}, request))
