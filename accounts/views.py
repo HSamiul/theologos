1a25ab55ed6639e63805e4a1474479b33ed10c61
HSamiul <samiulh@andrew.cmu.edu>
2023-02-25 17:52:07 -0500

    fixed bug where account wasn't being created after pressing submit

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.http import HttpResponse
 
 def register(request):
-    if (request.POST == 'POST'):
-        form = UserCreationForm()
+    if request.method == 'POST':  
+        form = UserCreationForm(request.POST) # create form with POST data
         
-        if form.is_valid(): # how could it not be valid if I'm instiating it myself?
-            form.save()
-    else:
-        form = UserCreationForm()
-    
-    context = { 'form': form }
-    
+        if form.is_valid():
+            form.save()  
+        messages.success(request, 'Account created successfully')  
+  
+    else:  
+        form = UserCreationForm()  
+    context = {  
+        'form':form,
+    }  
     return render(request, 'accounts/register.html', context)
