072131170e333dcc30812a043f7eba3d0fe898d8
HSamiul <samiulh@andrew.cmu.edu>
2023-02-25 18:03:31 -0500

    documented register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    if request.method == 'POST':  
-        form = UserCreationForm(request.POST) # create form with POST data
+    if request.method == 'POST': # if this URL was requested via a POST method...
+        form = UserCreationForm(request.POST) # create form with the POST data
         
-        if form.is_valid():
+        if form.is_valid(): # validate and save the form (account)
             form.save()  
-        messages.success(request, 'Account created successfully')  
+            messages.success(request, 'Account created successfully') # Flash a success message
+            return HttpResponse('Created your account!') # TODO: Redirect to account page
   
-    else:  
+    else:  # URL was requested via GET, so just display the form
         form = UserCreationForm()  
+        
     context = {  
         'form':form,
-    }  
+    }
+      
     return render(request, 'accounts/register.html', context)
