0f32507a59061ee768cc7abdd01121dbb29e2102
Alice Hong <alicehon@andrew.cmu.edu>
2023-03-01 13:00:40 -0500

    Changed database to Postgres with port and user info.

--- theologos/settings.py
+++ theologos/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'theologos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'theologosdb',
+        'USER': 'theologos_usr',
+        'PASSWORD': 'theologos373',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
