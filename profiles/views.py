f8d865140e47818dbcdad42683b6528e3a6692c0
Sunny Liang <sunnyliang6@gmail.com>
2023-03-11 03:58:34 -0500

    Created a ModelForm and connected it to the project to manually test the Profile field validators (#15)

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+from .models import Profile
+from .forms import ProfileForm
+
+def profile(request):
+    if request.method == 'POST':
+        # profile = Profile.objects.get(user=request.user)
+        # form = ProfileForm(request.POST, instance=profile)
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile created successfully')
+            return HttpResponse('Created your profile!')
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profiles/profiles.html', {'form': form})
