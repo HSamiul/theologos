8b9fcf7c1f11251a3f826cf43e9ff09b9360cc12
Sunny Liang <sunnyliang6@gmail.com>
2023-03-11 18:20:46 -0500

    Wrote test cases for Comment and Vote models, created migration for models, and added commentary app to INSTALLED_APPS (#18)

---
+++ commentary/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from accounts.models import User
+from profiles.models import Profile
+from .models import Post, Comment, Vote
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='testpass')
+        self.profile = Profile.objects.create(user=User.objects.get(id=1), faith_tradition='PENT')
+        self.post = Post.objects.create(author=self.profile, title='Test Post', text='Test post content')
+        self.comment = Comment.objects.create(author=self.profile, text='Test comment', post=self.post)
+
+    def test_add_child_comment(self):
+        child_comment = Comment.objects.create(author=self.profile, text='Test child comment', post=self.post)
+        self.comment.add_child_comment(child_comment)
+        self.assertEqual(child_comment.parent_comment, self.comment)
+
+    def test_get_child_comments(self):
+        child_comment_1 = Comment.objects.create(author=self.profile, text='Test child comment 1', post=self.post)
+        child_comment_2 = Comment.objects.create(author=self.profile, text='Test child comment 2', post=self.post)
+        self.comment.add_child_comment(child_comment_1)
+        self.comment.add_child_comment(child_comment_2)
+        child_comments = self.comment.get_child_comments()
+        self.assertIn(child_comment_1, child_comments)
+        self.assertIn(child_comment_2, child_comments)
+
+class VoteTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='testpass')
+        self.profile = Profile.objects.create(user=User.objects.get(id=1), faith_tradition='PENT')
+        self.post = Post.objects.create(author=self.profile, title='Test Post', text='Test post content')
+        self.comment = Comment.objects.create(author=self.profile, text='Test comment', post=self.post)
+
+    def test_add_post_vote(self):
+        vote = Vote.objects.create(voter=self.profile, post=self.post, vote_type='up')
+        self.assertEqual(vote.voter, self.profile)
+        self.assertEqual(vote.post, self.post)
+        self.assertEqual(vote.vote_type, 'up')
+
+    def test_add_comment_vote(self):
+        vote = Vote.objects.create(voter=self.profile, post=self.post, comment=self.comment, vote_type='down')
+        self.assertEqual(vote.voter, self.profile)
+        self.assertEqual(vote.comment, self.comment)
+        self.assertEqual(vote.vote_type, 'down')
--- theologos/settings.py
+++ theologos/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'accounts',
-    'profiles'
+    'profiles',
+    'commentary',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
