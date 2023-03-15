0f1d7786e77ecf1305d1687615255248aa7212fd
Alice Hong <alicehon@andrew.cmu.edu>
2023-03-14 21:57:16 -0400

    Removed Testament table and added field descriptions.

--- bible/models.py
+++ bible/models.py
@@ -1,22 +1,66 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-# TODO change relevant fields to enums instead of charfields
-
-class Testament(models.Model):
-    original_title = models.CharField(max_length=30)
-    original_lang_id = models.CharField(default='en', max_length=10)
 
 class Book(models.Model):
-    testament = models.ForeignKey(Testament, on_delete=models.CASCADE)
-    original_title = models.CharField(max_length=30)
-    original_lang_id = models.CharField(default='en', max_length=10)
+
+    # [Primary key] The book number (e.g. 01)
+    number = models.IntegerField(default='01', primary_key=True, validators=[MinLengthValidator(2), MaxLengthValidator(2)])
+
+    class BookTitle(models.TextChoices):
+        '''
+        An enumeration type that defines the titles for the books of the Bible 
+        (in English).
+
+        Note: Abbreviations taken from https://www.logos.com/bible-book-abbreviations
+        '''
+
+        # TODO complete the rest of the books
+
+        # OLD TESTAMENT
+        GENESIS = 'gen', 'Genesis'
+        EXODUS = 'ex', 'Exodus'
+        LEVITICUS = 'lev', 'Leviticus'
+        
+        # NEW TESTAMENT
+        PHILIPPIANS = 'phil', 'Philippians'
+
+
+    # The title of the book (e.g. "Genesis")
+    original_title = models.CharField(max_length=7, choices=BookTitle.choices)
+
+    # The language ID of the original title (e.g. 'en' for English)
+    #original_lang_id = models.CharField(default='en', max_length=10)
+
+    # Enumeration of testaments
+    TESTAMENTS = (
+        ('old', 'Old Testament'),
+        ('new', 'New Testament'),
+    )
+
+    # The testament the book belongs to
+    testament = models.CharField(max_length=3, choices=TESTAMENTS)
+
 
 class Chapter(models.Model):
+
+    # The book the chapter belongs to
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    number = models.IntegerField()
+
+    # The chapter number (e.g. 012)
+    number = models.IntegerField(validators=[MinLengthValidator(3), MaxLengthValidator(3)])
+
 
 class Verse(models.Model):
+
+    # The chapter the verse belongs to
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
-    number = models.IntegerField()
-    original_text = models.CharField(max_length=200)
-    original_lang_id = models.CharField(default='en', max_length=10)
+
+    # The verse number (e.g. 012)
+    number = models.IntegerField(validators=[MinLengthValidator(3), MaxLengthValidator(3)])
+
+    # The text of the verse
+    original_text = models.CharField(max_length=500)
+
+    # The language ID of the original text (e.g. 'en' for English)
+    #original_lang_id = models.CharField(default='en', max_length=10)
