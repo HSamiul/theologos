76affd128f956a9429e0657b30b86172f9a78916
HSamiul <samiulh@andrew.cmu.edu>
2023-03-18 18:28:02 -0400

    changed ProfileForm -> ProfileCreationForm

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from .models import Profile
 
 # TODO: Create a form that combines the User and Profile models.
-class ProfileForm(ModelForm):
+class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['dob', 'faith_tradition', 'phone']
