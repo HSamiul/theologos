69f1ee937e4e048c56f8e3dde4f983b1af3695c5
HSamiul <samiulh@andrew.cmu.edu>
2023-03-19 13:58:27 -0400

    added TODO to update validate_phone

--- profiles/validators.py
+++ profiles/validators.py
@@ -8,6 +8,7 @@ def validate_dob(value):
         raise ValidationError("Not a valid date of birth")
     return value
 
+# TODO: Update validate_phone to accept all phone numbers and not 05-XXX-XXX-XXXX
 validate_phone = RegexValidator(
     regex=r'^(05)\d{9}$',
     message='Phone number must be entered in the format: 05999999999'
