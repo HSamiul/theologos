b0acaed7771fc00943b7e619e74b566bf2307c4a
HSamiul <samiulh@andrew.cmu.edu>
2023-03-26 14:00:36 -0400

    Created templates/registrations, which is where Django looks for auth templates. Added login.html.

--- theologos/settings.py
+++ theologos/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'theologos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
