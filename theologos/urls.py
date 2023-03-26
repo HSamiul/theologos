70ebb093c6bcb1697e0b96edd386ccda663d4239
HSamiul <samiulh@andrew.cmu.edu>
2023-03-26 13:44:51 -0400

    added built-in auth urls to the accounts path

--- theologos/urls.py
+++ theologos/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include('accounts.urls')),
-    path("bible/", include('bible.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('bible/', include('bible.urls')),
     path('profiles/', include('profiles.urls'))
 ]
