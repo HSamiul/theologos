e75628840a2383a02f09bbaeac73a30ee54c4ad8
Alice Hong <alicehon@andrew.cmu.edu>
2023-03-28 01:06:52 -0400

    Modified view function to pass in a list of verses sorted by id, instead of the entire chapter object.

--- bible/views.py
+++ bible/views.py
@@ -5,4 +5,5 @@ from .models import Book, Chapter
 def index(request, book_id, chapter_num): # TODO add version_id in the future
     book = get_object_or_404(Book, pk=book_id)
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
-    return render(request, 'bible/index.html', {'book': book, 'chapter': chapter})
+    verses = chapter.verse_set.order_by("id")
+    return render(request, 'bible/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
