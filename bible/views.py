9536df8506b3a4a2eb217084cfac93e9340b2ccd
HSamiul <samiulh@andrew.cmu.edu>
2023-04-02 13:42:18 -0400

    added Verse foreign key to Post

--- bible/views.py
+++ bible/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Book, Chapter
+from commentary.models import Post
 
 def index(request, book_id, chapter_num): # TODO add version_id in the future
     book = get_object_or_404(Book, pk=book_id)
@@ -14,4 +15,13 @@ def commentary(request, book_id, chapter_num, verse_num):
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
     verses = chapter.verse_set.order_by("id")
     
-    return render(request, 'commentary/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
+    posts = get_object_or_404(Post, verse=verse_num)
+    
+    context = {
+        'book': book, 
+        'chapter_num': chapter_num, 
+        'verses': verses,
+        'posts': posts
+        }
+    
+    return render(request, 'commentary/index.html', context)
