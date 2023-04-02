1d8a268742426f20521ae2163f2de65c95666356
HSamiul <samiulh@andrew.cmu.edu>
2023-04-02 14:08:39 -0400

    experimental change to just allow you to view a specific verse by modifying URL

--- bible/views.py
+++ bible/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Book, Chapter
+from .models import Book, Chapter, Verse
 from commentary.models import Post
 
 def index(request, book_id, chapter_num): # TODO add version_id in the future
@@ -10,18 +10,17 @@ def index(request, book_id, chapter_num): # TODO add version_id in the future
     
     return render(request, 'bible/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
 
-def commentary(request, book_id, chapter_num, verse_num):
-    book = get_object_or_404(Book, pk=book_id)
+def commentary(request, book_id, chapter_num, verse_num):    
+    print(chapter_num)
+    print(verse_num)
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
-    verses = chapter.verse_set.order_by("id")
+    verse = get_object_or_404(Verse, chapter=chapter, number=verse_num)
+    # posts = get_object_or_404(Post, verse=verse_num)
     
-    posts = get_object_or_404(Post, verse=verse_num)
     
     context = {
-        'book': book, 
-        'chapter_num': chapter_num, 
-        'verses': verses,
-        'posts': posts
+        'verse': verse,
+        # 'posts': posts
         }
     
     return render(request, 'commentary/index.html', context)
