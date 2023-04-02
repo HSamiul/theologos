b10c2db8abce4dedfe3e3d5bd7b6d3a6e4364bda
HSamiul <samiulh@andrew.cmu.edu>
2023-04-02 16:57:19 -0400

    added simple authentication conditional to commentary view to prepare for adding in PostCreationForm

--- bible/views.py
+++ bible/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from .models import Book, Chapter, Verse
 from commentary.models import Post
 
+from commentary.forms import PostCreationForm
+
 def index(request, book_id, chapter_num): # TODO add version_id in the future
     book = get_object_or_404(Book, pk=book_id)
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
@@ -11,12 +13,17 @@ def index(request, book_id, chapter_num): # TODO add version_id in the future
     return render(request, 'bible/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
 
 def commentary(request, book_id, chapter_num, verse_num):    
-    print(chapter_num)
-    print(verse_num)
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
     verse = get_object_or_404(Verse, chapter=chapter, number=verse_num)
-    posts = verse.post_set.order_by("id")
+    posts = verse.post_set.order_by('id')
     
+    # if request.method == 'POST':
+    #     # use POST data to populate form fields
+    #     postCreationForm = PostCreationForm(request.POST)
+    #     postCreationFormValid = postCreationForm.is_valid()
+        
+    #     if postCreationFormValid:
+    #         post.
     
     context = {
         'verse': verse,
