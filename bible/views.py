626626c73ed8fd352f7f09f10bcedad1cec67743
HSamiul <samiulh@andrew.cmu.edu>
2023-04-02 12:51:36 -0400

    created url, view, and template for commentary

--- bible/views.py
+++ bible/views.py
@@ -6,4 +6,12 @@ def index(request, book_id, chapter_num): # TODO add version_id in the future
     book = get_object_or_404(Book, pk=book_id)
     chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
     verses = chapter.verse_set.order_by("id")
+    
     return render(request, 'bible/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
+
+def commentary(request, book_id, chapter_num, verse_num):
+    book = get_object_or_404(Book, pk=book_id)
+    chapter = get_object_or_404(Chapter, book=book_id, number=chapter_num)
+    verses = chapter.verse_set.order_by("id")
+    
+    return render(request, 'commentary/index.html', {'book': book, 'chapter_num': chapter_num, 'verses': verses})
