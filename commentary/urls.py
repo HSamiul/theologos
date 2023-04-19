121f8e4c56d829f0511058e2f05384cd4065ec57
Alice Hong <alicehon@andrew.cmu.edu>
2023-04-19 15:06:57 -0400

    Finished changing post update view to be part of split view

--- commentary/urls.py
+++ commentary/urls.py
@@ -5,4 +5,5 @@ app_name = 'commentary'
 urlpatterns = [
     path('post/<slug:pk>', views.PostDetailView.as_view(), name='post-detail'), # i.e. /post/<post_id>
     path('post/<slug:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'), # i.e. /post/<post_id>/delete
+    path('post/<slug:pk>/edit', views.PostUpdateView.as_view(), name='post-edit'), # i.e. /post/<post_id>/edit
 ]
--- commentary/views.py
+++ commentary/views.py
@@ -1,5 +1,4 @@
 from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
@@ -14,14 +13,6 @@ Notes:
 - get_success_url is the URL that you get redirected to after the update/delete
 - test_func is used by the UserPassesTestMixin to decide if a user is allowed to access the view
 '''
-
-class PostListView(ListView):
-    model = Post
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["books"] = Book.objects.all()
-        return context
 
 class PostDetailView(DetailView):
     model = Post
@@ -43,6 +34,25 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return reverse_lazy('bible:index', kwargs={'verse_id': post.verse.get_id() })
     
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user.id == post.author.user.id
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name_suffix = '_update_form' # searches for the "post_update_form.html" template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = Book.objects.all()
+        return context
+    
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse_lazy('bible:index', kwargs={'verse_id': post.verse.get_id() })
+
     def test_func(self):
         post = self.get_object()
         return self.request.user.id == post.author.user.id
