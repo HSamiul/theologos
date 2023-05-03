00fc9b749bfdbd1677010b12b4f5084710dfed24
HSamiul <samiulh@andrew.cmu.edu>
2023-05-02 20:06:50 -0400

    updated URL for UserUpdateView to not include <int:pk>

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', register, name='register'), # /accounts/register/
     path('detail/', UserDetailView.as_view(), name='detail'), # /accounts/detail/
-    path('<int:pk>/edit/', UserUpdateView.as_view(), name='edit'), # /accounts/1/edit/
+    path('edit/', UserUpdateView.as_view(), name='edit'), # /accounts/edit/
     path('delete/', UserDeleteView.as_view(), name='delete') # /accounts/delete/
 ]
